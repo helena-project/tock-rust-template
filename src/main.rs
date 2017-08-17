ac8fe15856938a12cd0347f92d47b59d55f27696
Branden Ghena <brghena@umich.edu>
2017-08-17 15:40:23 -0700

    More minimal working example

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,10 @@ extern crate alloc;
 extern crate tock;
 
 use alloc::fmt::Write;
-use tock::console::Console;
 
+#[inline(never)]
 fn main() {
-    let mut console = Console::new();
-    for i in 0.. {
-        write!(&mut console, "Starting {}\n", i).unwrap();
-        tock::timer::delay_ms(1000);
-    }
+    let mut console = tock::console::Console::new();
+    write!(&mut console, "Tock App\n").unwrap();
 }
 
