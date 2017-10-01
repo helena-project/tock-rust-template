c8e38804e9e7d63421d3810a581e2c111c9e1eb2
James Munns <james.munns@gmail.com>
2017-10-01 03:04:46 +0200

    Proof of Concept work

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,29 @@ extern crate alloc;
 extern crate tock;
 
 use alloc::fmt::Write;
+use tock::console::Console;
+use tock::ipc::ble_ess::{self, ReadingType};
+use tock::sensors::*;
+use tock::ipc::Client;
+use alloc::String;
 
-#[inline(never)]
 fn main() {
     let mut console = tock::console::Console::new();
-    write!(&mut console, "Tock App\n").unwrap();
+    let mut client = Client::new(
+            String::from("org.tockos.services.ble-led-ctrl")).unwrap();
+    let buffer = client.share(5).unwrap();
+
+
+    tock::timer::delay_ms(5000);
+
+    
+    write!(&mut console, "Init\n");
+
+    client.led_register().unwrap();
+
+    loop {
+        tock::timer::delay_ms(10000);
+        client.ping();
+    }
 }
 
